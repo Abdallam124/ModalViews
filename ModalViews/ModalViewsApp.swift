cf68daf157cdc7bfd107e35981c2a3389401a413
Abdalla Mohammed <Abdallamohammed99@icloud.com>
2025-03-23 17:08:18 -0400

    Initial Commit

---
+++ ModalViews/ModalViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalViewsApp.swift
+//  ModalViews
+//
+//  Created by Abdalla Mohammed on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ModalViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
